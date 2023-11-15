8098885498bece691e1592de80014618b5ae0ff9
Luan Fernando <30829901+LuanFernando@users.noreply.github.com>
2023-11-15 12:50:19 -0300

    Add files via upload

---
+++ public/js/login.js
@@ -0,0 +1,21 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // Verifica se o tamanho da tela é pequeno, indicando um dispositivo móvel
+    if (window.innerWidth <= 768 && /Mobi|Android/i.test(navigator.userAgent)) {
+        var formLogin = $("#form-login");
+        formLogin.animate({height:'100vh'}, "slow");
+        formLogin.animate({width:'100vw'}, "slow");
+
+        var boxLogin = $('.box-login');
+        boxLogin.css ('display','flex');
+    } else {
+        var formLogin = $("#form-login");
+        formLogin.animate({height:'400px'}, "slow");
+        formLogin.animate({width:'400px'}, "slow");
+
+        var boxLogin = $('.box-login');
+        boxLogin.css ('display','flex');
+
+    }
+});
+
+    
